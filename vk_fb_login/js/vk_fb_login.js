213228a3d0bb87ff1e1cd8c423d8ca33f9e985c8
Anton Trokhymenko <antroxim@gmail.com>
2012-10-10 15:14:31 +0300

    VK-FB Login module for Drupal 7.x
    Drupal module for login trough Vk and Fb social networks

---
+++ vk_fb_login/js/vk_fb_login.js
@@ -0,0 +1,14 @@
+/*
+
+function authInfo(response) {
+  if (response.session) {
+//    alert('user: '+response.session.mid);
+  } else {
+//    alert('not auth');
+  }
+}
+// VK.Auth.getLoginStatus(authInfo);
+*/
+// VK.UI.button('vk_login');
+
+//VK.Widgets.Auth("vk_login", {width: "150px", authUrl: '/vk/login?'});
